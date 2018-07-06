deb003483f4bb2458661a5c72920def0dabc9518
Steve Morris <sleeve@users.noreply.github.com>
2018-07-05 18:55:59 -0700

    initial commit

---
+++ randy/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  randy
+//
+//  Created by Steve Morris on 7/5/18.
+//  Copyright © 2018 Steve Morris. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
