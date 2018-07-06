aef26c2767475dad3a09392ab86347e14669c90a
Steve Morris <sleeve@users.noreply.github.com>
2018-07-05 20:10:34 -0700

    some dirty randomness test code

--- randy/main.swift
+++ randy/main.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 
-print("Hello, World!")
+var zeros: Int = 0
+var ones: Int = 0
 
+for _ in 1...1000000 {
+    let randomBit = Int.random(in: 0...1)
+//    print(randomBit)
+    
+    if randomBit == 0 {
+        zeros += 1
+    }
+    else {
+        ones += 1
+    }
+}
+
+print("zeros = \(zeros) and ones = \(ones)")
